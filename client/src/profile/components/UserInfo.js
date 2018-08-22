5e7b062a9d3fb50c20ce2a0574faf156d273fc93
deerek <derek.bodily@gmail.com>
2018-08-21 20:39:22 -0600

    fixed a couple conflicts and got everything working from the last merge

--- client/src/profile/components/UserInfo.js
+++ client/src/profile/components/UserInfo.js
@@ -15,13 +15,9 @@ function UserInfo(props) {
                 <Avatar imgUrl={props.avatar}/>
                 <div className="user-names-wrapper">
                     <h3>{props.firstName} {props.lastName}</h3>
-                    <h5>{props.username}</h5>
+                    <h5>@{props.username}</h5>
                 </div>
             </div>
-
-            <Avatar />
-            <h3>{props.firstName} {props.lastName}</h3>
-            <h5>@{props.username}</h5>
 
             <p>{props.bio}</p>
             {!props.public && <Link to="/edit-profile">
@@ -31,13 +27,14 @@ function UserInfo(props) {
             <p>Followers: {props.followers.length}</p>
             <p>Following: {props.following.length}</p>
             {/* click on follow button, shoot off put request which contains the ids of the athlete/brand and user */}
-            {props.public && <FollowButton follow={props.follow} />}
-            {props.public && props.userType === "athlete" && props.auth.user.userType === "brand" && <SponsorButton />}
-
+            
             <div className="profile-buttons-wrapper">
                 {/* sponsor only if logged in user is brand and profile is athlete*/}
-                {props.public && <FollowButton className="follow-button" />}
-                {props.public && props.userType === "athlete" && props.auth.user.userType === "brand" && <SponsorButton className="sponsor-button" />}
+                {props.public && <FollowButton follow={props.follow} />}
+            {props.public && props.userType === "athlete" && props.auth.user.userType === "brand" && <SponsorButton />}
+
+                {/* {props.public && <FollowButton className="follow-button" />}
+                {props.public && props.userType === "athlete" && props.auth.user.userType === "brand" && <SponsorButton className="sponsor-button" />} */}
             </div>
 
         </div>
